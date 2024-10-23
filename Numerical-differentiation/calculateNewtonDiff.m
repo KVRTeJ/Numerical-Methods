8f6d799c52ff6aa3b1fe65bc374d681ecbef8718
KVRTeJ <bb.sssss@inbox.ru>
2024-10-23 20:20:39 +0700

    functioin calculateNewtonDiff and calculateNewtonDiffTwice are implemented

--- Numerical-differentiation/calculateNewtonDiff.m
+++ Numerical-differentiation/calculateNewtonDiff.m
@@ -6,9 +6,6 @@ function result = calculateNewtonDiff(abscissaAxis, ordinateAxis, x)
         coef(i) = (x - abscissaAxis(i));
     end
 
-    result = splitDifference(1, 1) + (coef(1) + coef(2)) * splitDifference(1, 2);
-
-    % result = splitDifference(1, 1) + (coef(1) + coef(2)) * splitDifference(1, 2) + (coef(1) * coef(2) + coef(1) * coef(3) + coef(2) * coef(3)) * splitDifference(1, 3) + ...
-    %     (coef(1) * (coef(2) * (coef(3) + coef(4)) + coef(3) * coef(4)) + coef(2) * coef(3) * coef(4)) * splitDifference(1, 4);
-
+    result = splitDifference(1, 1) + (coef(1) + coef(2)) * splitDifference(1, 2) + (coef(1) * coef(2) + coef(1) * coef(3) + coef(2) * coef(3)) * splitDifference(1, 3) + ...
+        (coef(1) * (coef(2) * (coef(3) + coef(4)) + coef(3) * coef(4)) + coef(2) * coef(3) * coef(4)) * splitDifference(1, 4);
 end
--- Numerical-differentiation/main.m
+++ Numerical-differentiation/main.m
@@ -1,37 +1,26 @@
 clear all
 clc
 
-x = [0 0.5 1];
-y = [1.5 1.1 1.7];
+yAxis = [1.000 0.195 -0.332 0.849 0.182];
+xAxis = [0.000 1.500 3.100 3.700 4.500];
+xPoint = -0.137;
+xRange = linspace(xAxis(1), xAxis(length(xAxis)), 10000);
+trueFunction = cos(xRange .* log(xRange + 1));
 
-temp = calculateNewtonDiff(x, y, 1);
-disp(temp)
+yAxisDiff = zeros(1, length(yAxis));
+for i = 1:1:length(yAxisDiff)
+    yAxisDiff(i) = - (log(xAxis(i) + 1) + xAxis(i) / (xAxis(i) + 1) ) * sin(xAxis(i) * log(xAxis(i) + 1));
+end
 
-% yAxis = [1.000 0.195 -0.332 0.849 0.182];
-% xAxis = [0.000 1.500 3.100 3.700 4.500];
-% xPoint = -0.137;
-% xRange = linspace(xAxis(1), xAxis(length(xAxis)), 10000);
-% trueFunction = cos(xRange .* log(xRange + 1));
-% 
-% yAxisDiff = zeros(1, length(yAxis));
-% for i = 1:1:length(yAxisDiff)
-%     yAxisDiff(i) = - (log(xAxis(i) + 1) + xAxis(i) / (xAxis(i) + 1) ) * sin(xAxis(i) * log(xAxis(i) + 1));
-% end
-% 
-% fprintf("y' ->");
-% for i = 1:1:length(yAxis)
-%         fprintf(" %f", yAxisDiff(i));
-% end
-% fprintf("\n");
-% 
-% 
-% 
-% temp = calculateNewtonDiff(xAxis, yAxis, 3.7);
-% disp(temp)
-% 
-% 
-% figure
-%     plot(xAxis, yAxis)
-%     hold on
-% 
-%     plot(xRange, yRange, LineWidth=1.5)
+fprintf("y' ->");
+for i = 1:1:length(yAxis)
+        fprintf(" %f", yAxisDiff(i));
+end
+fprintf("\n");
+
+
+figure
+    plot(xAxis, yAxis)
+    hold on
+
+    plot(xRange, yRange, LineWidth=1.5)
