220264e29de460fff2d9c23b0d7b0110430c4a68
KVRTeJ <bb.sssss@inbox.ru>
2024-10-29 01:32:52 +0700

    refactor main.m and beginning of the implementation of the Simpson method

--- Numerical-integration/Calculate-integral-value/calculateIntegralValueBySimpson.m
+++ Numerical-integration/Calculate-integral-value/calculateIntegralValueBySimpson.m
@@ -1,4 +1,19 @@
-function [result, nodeCount] = calculateIntegralValueBySimpson(from, to)
-    result = -1;
-    nodeCount = -1;
+function [result, nodeCount] = calculateIntegralValueBySimpson(from, to, accuracy)
+    x = 0;
+    result = myFunction(x); %this function can be passed through arguments if there is a Symbolic Math Toolbox
+    segmentCount = 4;
+
+    coef = 0; % (b - a) / (3 * m)
+    sumEven = 0;
+    sumOdd = 0;
+    for i = 2:1:segmentCount
+        step = (to - from) / segmentCount;
+
+        %TODO: loop for odd
+        %TODO: loop for even
+
+        coef = (to - from) / (3 * segmentCount);
+        result = coef * (myFunction(from) + myFunction(to) + sumEven + sumOdd);
+        nodeCount = segmentCount + 1;
+    end
 end
--- Numerical-integration/Calculate-integral-value/main.m
+++ Numerical-integration/Calculate-integral-value/main.m
@@ -2,8 +2,17 @@ clc
 clear all
 
 interval = [-1 1];
-fun = interval.^2 .* log(interval + 2);
+fun = myFunction(interval);
+accuracy = 0.0005;
 
+calculateIntegralValueBySimpson(interval(1), interval(2), accuracy);
+
+x = 0;
+fun = calculateIntegralValueBySimpson(x);
 disp(fun)
+x = 1;
+fun = x^2 * log(x + 2);
+disp(fun)
+
 
 
