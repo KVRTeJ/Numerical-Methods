49bd3fe24d5cf589eaa760f917584b2d39b51e23
KVRTeJ <bb.sssss@inbox.ru>
2024-10-29 03:20:27 +0700

    beginning of the implementation of the Runge estimate

--- Numerical-integration/Calculate-integral-value/calculateIntegralValueBySimpson.m
+++ Numerical-integration/Calculate-integral-value/calculateIntegralValueBySimpson.m
@@ -1,27 +1,29 @@
 function [result, nodeCount] = calculateIntegralValueBySimpson(from, to, accuracy)
-    x = 0;
-    result = myFunction(x); %this function can be passed through arguments if there is a Symbolic Math Toolbox
+    result = 1; %this function can be passed through arguments if there is a Symbolic Math Toolbox
     segmentCount = 2;
+    dataResult = 0;
+    nodeCount = segmentCount + 1;
 
-    leftHandSide = myFunction(from);
-    rightHandSide = myFunction(to);
-    coef = 0; % (b - a) / (3 * m)
     sumEven = 0;
     sumOdd = 0;
-    for i = 2:1:segmentCount
+    coef = 0; % (b - a) / (3 * m)
+    leftHandSide = myFunction(from);
+    rightHandSide = myFunction(to);
+    while abs((result - dataResult) / 15) > accuracy%FIXME
         step = (to - from) / segmentCount;
 
-        %TODO: loop for sum odd
-        for j = (from + step * 2):(step * 2):(to - step)
-            sumOdd = sumOdd + myFunction(j);
-        end
-        %TODO: loop for sum even
+        sumOdd = sumOdd + sumEven;
         for j = (from + step):(step * 2):(to - step * 2)
+            disp(j)
             sumEven = sumEven + myFunction(j);
         end
         
         coef = (to - from) / (3 * segmentCount);
+        dataResult = result;
         result = coef * (leftHandSide + rightHandSide + sumEven + sumOdd);
         nodeCount = segmentCount + 1;
+
+        sumEven = 0;
+        segmentCount = segmentCount * 2
     end
 end
--- Numerical-integration/Calculate-integral-value/main.m
+++ Numerical-integration/Calculate-integral-value/main.m
@@ -5,12 +5,9 @@ interval = [-1 1];
 fun = myFunction(interval);
 accuracy = 0.0005;
 
-fun = myFunction(-1)
-fun = myFunction(0)
-fun = myFunction(1)
-
-result = calculateIntegralValueBySimpson(interval(1), interval(2), accuracy);
+[result, temp] = calculateIntegralValueBySimpson(interval(1), interval(2), accuracy);
 disp(result)
+disp(temp)
 
 
 
