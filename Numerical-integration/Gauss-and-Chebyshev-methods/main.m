e04166ef51dc3634f08d958c7b983137f69ec58d
KVRTeJ <bb.sssss@inbox.ru>
2024-11-05 01:50:22 +0700

    update main

--- Numerical-integration/Gauss-and-Chebyshev-methods/main.m
+++ Numerical-integration/Gauss-and-Chebyshev-methods/main.m
@@ -3,7 +3,7 @@ clear all
 
 interval = [-1 1];
 fun = myFunction(interval);
-accuracy = 0.00000005;
+accuracy = 0.0005;
 
 [temp, m] = calculateByGauss(interval(1), interval(2), accuracy, 6);
 fprintf("\n%f\n", temp);
