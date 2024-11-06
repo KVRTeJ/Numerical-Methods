a9afa52d20dd1db7662e845f1722b2264126d013
KVRTeJ <bb.sssss@inbox.ru>
2024-11-06 17:14:32 +0700

    the simplest Monte Carlo method is implemented

--- Numerical-integration/Monte-Carlo-methods/simplestMethod.m
+++ Numerical-integration/Monte-Carlo-methods/simplestMethod.m
@@ -1,10 +1,23 @@
 function result = simplestMethod(from, to, accuracy)
 
-    n = 1000;
+    size = 1000;
 
-    epsilon = from + (to - from) * rand(n, 1);
+    epsilon = from + (to - from) * rand(size, 1);
     eta = myFunction(epsilon);
 
-    integralValue = (to - from) / n * sum(eta)
+    integralValue = (to - from) / size * sum(eta);
+    dispersion = (to - from) ^ 2 / size * sum(eta.^2) - integralValue ^ 2;
 
+    sizeStep = 100;
+    oldIntegralValue = 0;
+    while 3 * sqrt(dispersion / size) > accuracy
+        epsilon = from + (to - from) * rand(sizeStep, 1);
+        eta = myFunction(epsilon);
+
+        oldIntegralValue = integralValue;
+        integralValue = 1 / (size + sizeStep) * (size * integralValue + (to - from) * sum(eta))
+        dispersion = 1 / (size + sizeStep) * (size * dispersion + size * oldIntegralValue^2 + (to - from)^2 * sum(eta.^2)) - integralValue ^ 2;
+
+        size = size + sizeStep;
+    end
 end
