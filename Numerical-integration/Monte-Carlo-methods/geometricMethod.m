2a603a1c6582a5cc5e209fd9297df7122cc2385b
KVRTeJ <bb.sssss@inbox.ru>
2024-11-06 20:54:49 +0700

    The geometric Monte Carlo method is implemented

--- Numerical-integration/Monte-Carlo-methods/geometricMethod.m
+++ Numerical-integration/Monte-Carlo-methods/geometricMethod.m
@@ -1,3 +1,19 @@
-function [result, size] = geometricMethod(from. to, accuracy)
+function [result, size] = geometricMethod(from, to, accuracy)
+    min = myFunction(from);
+    max = myFunction(to);
+
+    size = 25000;
+    alpha = 0.01;
+    while 1 / (2 * sqrt(alpha * size)) > accuracy
+        size = size + 10000;
+    end
+
+    xi = rand(size, 1);
+    eta = rand(size, 1);
+
+    favorableEvents = (myFunction(from + (to - from) .* xi) - min) ./ (max - min) >= eta;
+    sumFavorableEvents = sum(favorableEvents);
+
+    result = (max - min) * (to - from) * (sumFavorableEvents / size) + min * (to - from);
     
 end
