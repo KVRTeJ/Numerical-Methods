4fd7f01999c820ec626c109b4343a91157924888
KVRTeJ <bb.sssss@inbox.ru>
2025-02-26 17:58:01 +0700

    implemented Newton method. refactoring

--- Solving-systems-of-nonlinear-equations/main.m
+++ Solving-systems-of-nonlinear-equations/main.m
@@ -10,11 +10,9 @@ fprintf("cos(x - 1) + 4 * y - 1.5 = 0\n\n");
 
 firstFun = @(x, y) sin(y) + 2 * x - 2;
 secondFun = @(x, y) cos(x - 1) + 4 * y - 1.5;
-accuracy = 0.5 * 10^(-5);
+system = @(x, y) [firstFun(x, y), secondFun(x, y)];
 
-xRange = -pi:pi/100:pi;
-forInitialApproximation = -asin(2 - 2 * xRange);
-forInitialApproximation2 = (1.5/4 * (-cos(xRange - 1)/4));
+accuracy = 0.5 * 10^(-5);
 
 initialApproximation = [0.95 -0.09];
 
@@ -36,6 +34,30 @@ canonForm2 = @(x, y) (1.5 - cos(x - 1)) / 4;
     fprintf("cos(%f - 1) + 4 * %f - 1.5 = %f\n\n", x, y, res2);
 
 
+df_11 = @(x) 2;
+df_12 = @(y) cos(y);
+df_21 = @(x) -sin(x - 1);
+df_22 = @(y) 4;
+yakobiMatrix = @(x, y) [df_11(x), df_12(y); df_21(x), df_22(y)];
+
+    [result, steps] = newtonMethod(system, yakobiMatrix, initialApproximation, accuracy);
+    fprintf("Метод Ньютона\n");
+    fprintf("Начальное приближение (0,95;-0,09)\n");
+    fprintf("Точность %f\n", accuracy);
+    fprintf("Количетсво итераций - %d\n", steps);
+    
+    res1 = firstFun(result(1), result(2));
+    res2 = secondFun(result(1), result(2));
+    fprintf("Результат: (%f;%f)\n", result(1), result(2));
+    
+    fprintf("Проверка: \n");
+    fprintf("sin(%f) + 2 * %f - 2 = %f\n", result(2), result(1), res1);
+    fprintf("cos(%f - 1) + 4 * %f - 1.5 = %f\n\n", result(1), result(2), res2);
+
+
+xRange = -pi:pi/100:pi;
+forInitialApproximation = -asin(2 - 2 * xRange);
+forInitialApproximation2 = (1.5/4 * (-cos(xRange - 1)/4));
 
 figure
     plot(xRange, forInitialApproximation,...
--- Solving-systems-of-nonlinear-equations/newtonMethod.m
+++ Solving-systems-of-nonlinear-equations/newtonMethod.m
@@ -1,5 +1,30 @@
 function [result, steps] = newtonMethod(system, yakobiMatrix, initialApproximation, accuracy)
-    prevResult = initialApproximation - yakobiMatrix(res_p) ^ (-1) * system(initialApproximation); 
+    prevResult = [0, 0];
+    result = [0, 0];
 
-    result = prevResult -  yakobiMatrix(prevResult) ^ (-1) * system(prevResult); 
+    invertedYakobiMatrix = @(x, y) yakobiMatrix(x, y) ^ (-1);
+
+    currentIVM = invertedYakobiMatrix(initialApproximation(1), initialApproximation(2));
+    currentSystem = system(initialApproximation(1), initialApproximation(2));
+    prevResult(1) = initialApproximation(1) - currentIVM(1, 1) * currentSystem(1) - currentIVM(1, 2) * currentSystem(2);
+    prevResult(2) = initialApproximation(2) - currentIVM(2, 1) * currentSystem(1) - currentIVM(2, 2) * currentSystem(2);
+
+    currentIVM = invertedYakobiMatrix(prevResult(1), prevResult(2));
+    currentSystem = system(prevResult(1), prevResult(2));
+    result(1) = prevResult(1) - currentIVM(1, 1) * currentSystem(1) - currentIVM(1, 2) * currentSystem(2);
+    result(2) = prevResult(2) - currentIVM(2, 1) * currentSystem(1) - currentIVM(2, 2) * currentSystem(2);
+
+    steps = 1;
+
+    while(max(abs(result(1) - prevResult(1)), abs(result(2)) - prevResult(2)) >= accuracy)
+        prevResult = result;
+
+        currentIVM = invertedYakobiMatrix(prevResult(1), prevResult(2));
+        currentSystem = system(prevResult(1), prevResult(2));
+        result(1) = prevResult(1) - currentIVM(1, 1) * currentSystem(1) - currentIVM(1, 2) * currentSystem(2);
+        result(2) = prevResult(2) - currentIVM(2, 1) * currentSystem(1) - currentIVM(2, 2) * currentSystem(2);
+
+        steps = steps + 1;
+    end
+    
 end
