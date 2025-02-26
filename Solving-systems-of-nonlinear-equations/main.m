58b1c4dcf0fb47dfc7e3fb0301d6e3ac67851bae
KVRTeJ <bb.sssss@inbox.ru>
2025-02-26 15:45:15 +0700

    refactoring

--- Solving-systems-of-nonlinear-equations/main.m
+++ Solving-systems-of-nonlinear-equations/main.m
@@ -4,6 +4,10 @@ clear all;
 % sin(y) + 2x = 2
 % cos(x - 1) + 4y = 1,5
 
+fprintf("Исходная функция: \n")
+fprintf("sin(y) + 2 * x - 2 = 0\n");
+fprintf("cos(x - 1) + 4 * y - 1.5 = 0\n\n");
+
 firstFun = @(x, y) sin(y) + 2 * x - 2;
 secondFun = @(x, y) cos(x - 1) + 4 * y - 1.5;
 accuracy = 0.5 * 10^(-5);
@@ -17,15 +21,21 @@ initialApproximation = [0.95 -0.09];
 canonForm1 = @(x, y) 1 - sin(y) /2;
 canonForm2 = @(x, y) (1.5 - cos(x - 1)) / 4;
 
-[x, y, steps] = simpleIterations(canonForm1, canonForm2, initialApproximation, accuracy);
+    [x, y, steps] = simpleIterations(canonForm1, canonForm2, initialApproximation, accuracy);
+    fprintf("Метод простых итераций\n");
+    fprintf("Начальное приближение (0,95;-0,09)\n");
+    fprintf("Точность %f\n", accuracy);
+    fprintf("Количетсво итераций - %d\n", steps);
+    
+    res1 = firstFun(x, y);
+    res2 = secondFun(x, y);
+    fprintf("Результат: (%f;%f)\n", x, y);
+    
+    fprintf("Проверка: \n");
+    fprintf("sin(%f) + 2 * %f - 2 = %f\n", y, x, res1);
+    fprintf("cos(%f - 1) + 4 * %f - 1.5 = %f\n\n", x, y, res2);
 
 
-disp(x);
-disp(y);
-disp(steps);
-
-res = secondFun(x, y);
-disp(res);
 
 figure
     plot(xRange, forInitialApproximation,...
